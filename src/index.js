dc4753105b2c750b65cd19b4000184d5766dac6c
Conor <conorjm89@gmail.com>
2020-12-30 11:18:24 +0000

    Continue work on tab click logic

--- src/index.js
+++ src/index.js
@@ -7,3 +7,9 @@ homePage();
 document.querySelector('#home-tab').addEventListener('click', homePage);
 document.querySelector('#menu-tab').addEventListener('click', menuPage);
 document.querySelector('#contact-tab').addEventListener('click', contactPage);
+
+// Write the tab-switching logic inside of index.js. You should have event listeners
+// for each tab that wipes out the current contents and then runs the correct ‘tab module’
+// to populate it again.
+
+// Need to google how to do this
